2301b93cf0980567001001c357f39124f7d5c7a7
PaulaMihalcea <mihalceapaula@gmail.com>
2020-08-09 19:09:28 +0200

    Terrain and Perlin noise test

--- prova.js
+++ prova.js
@@ -2,128 +2,236 @@ import * as OC from "./three.js/examples/jsm/controls/OrbitControls.js"
 
 function main() {
 
-    const canvas = document.querySelector('#prova');
-    const renderer = new THREE.WebGLRenderer();
-    document.getElementById('prova').appendChild(renderer.domElement);
-  
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    const aspect = width / height;  // the canvas default
+  // Constants
+  const pi = Math.PI;
+  const rotSpeed = 3;
 
-    renderer.setSize(width, height);
+  // Canvas
+  const canvas = document.querySelector('#canvas');
 
-    // console.log(aspect); // OUTPUT
-    const fov = 90;
-    const near = 2;
-    const far = 10;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 5;
+  // Renderer
+  const renderer = new THREE.WebGLRenderer();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.getElementById('canvas').appendChild(renderer.domElement);
+
+  // Camera
+  const fov = 100;
+  const aspect = window.innerWidth / window.innerHeight;
+  const near = 0.1;
+  const far = 1000;
+
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+  camera.position.z = 5;
+
+  // Fake camera
+  let fakeCamera = camera.clone();
+  camera.copy(fakeCamera);
+
+  // Light
+  const lightColor = 0xFFFFFF;
+  const intensity = 1;
+
+  const light = new THREE.DirectionalLight(lightColor, intensity);
+
+  light.position.set(-1, 2, 4);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  scene.background = new THREE.Color(0xffffff);
+  scene.add(light);
   
-    const scene = new THREE.Scene();
-  
-    { // Light
-      const lightColor = 0xFFFFFF;
-      const intensity = 1;
-      const light = new THREE.DirectionalLight(lightColor, intensity);
-      light.position.set(-1, 2, 4);
-      scene.add(light);
-      scene.background = new THREE.Color(0xffffff);
+  // Cubes
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 2;
+  const boxGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  function makeInstance(boxGeometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
+
+    const cube = new THREE.Mesh(boxGeometry, material);
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
+  }
+
+  const cubes = [
+    makeInstance(boxGeometry, 0x44aa88, 0),
+    makeInstance(boxGeometry, 0x8844aa, -2),
+    makeInstance(boxGeometry, 0xaa8844, 2),
+  ];
+
+  // Plane
+  let makeQuad = function(geometry, position, addFace, verts) {
+    geometry.vertices.push(position);
+      
+    if (addFace) {
+      var index1 = geometry.vertices.length - 1;
+      var index2 = index1 - 1;
+      var index3 = index1 - verts;
+      var index4 = index1 - verts - 1;
+      
+      geometry.faces.push(new THREE.Face3(index2, index3, index1));
+      geometry.faces.push(new THREE.Face3(index2, index4, index3));
     }
-  
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 2;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-  
-    function makeInstance(geometry, color, x) {
-      const material = new THREE.MeshPhongMaterial({color});
-  
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-  
-      cube.position.x = x;
-  
-      return cube;
+  };
+
+  let planeSize = 1
+  let planeRes = 1
+  let geometry = new THREE.Geometry();
+  for (var i = 0; i <= planeRes; i++) {
+    for (var j = 0; j <= planeRes; j++) {
+      var z = j * planeSize;
+      var x = i * planeSize;
+      var position = new THREE.Vector3(x, 0, z);
+      var addFace = (i > 0) && (j > 0);
+      makeQuad(geometry, position, addFace, planeRes + 1);
     }
+  }
+  geometry.computeFaceNormals();
+  geometry.normalsNeedUpdate = true;
 
+  scene.add(geometry);
 
+  // Sphere
+  let sphereGeometry = new THREE.SphereGeometry(0.8, 128, 128);
+  let sphereMaterial = new THREE.MeshNormalMaterial();
+  let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.position.y += 2
+  scene.add(sphere);
 
+  // Generic geometry
+  //var texture = new THREE.TextureLoader().load('uv_texture.jpg'); // Texture loading
+  let genGeometry = new THREE.Geometry();
+  //let genMaterial = new THREE.MeshNormalMaterial({map: texture}); // Textured material
+  let genMaterial = new THREE.MeshNormalMaterial();
+  let genMesh = new THREE.Mesh(genGeometry, genMaterial);
+  genMesh.position.y -= 2.5
 
-    /******************************/
-    
-    const planeWidth = 1
-    const planeHeight = 1
-    const planeDepth = 2
-    const planeGeometry = new THREE.BoxGeometry(planeWidth, planeHeight, planeDepth);
-    const planeMaterial = new THREE.MeshPhongMaterial({color: 0xffff00, side: THREE.DoubleSide});
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  genGeometry.vertices.push(new THREE.Vector3( 1, -1, 0));
+  genGeometry.vertices.push(new THREE.Vector3(-1,  1, 0));
+  genGeometry.vertices.push(new THREE.Vector3(-1, -1, -1));
+  genGeometry.vertices.push(new THREE.Vector3( 1,  1, -1));
 
-    scene.add(plane)
+  genGeometry.faces.push(new THREE.Face3(0, 1, 2));
+  var uvs1 = [new THREE.Vector2(1, 0), new THREE.Vector2(0, 1), new THREE.Vector2(0, 0)];
+  genGeometry.faceVertexUvs[0].push(uvs1); //remember faceVertexUvs is an array of arrays
+  genGeometry.faces.push(new THREE.Face3(0, 3, 1));
+  var uvs2 = [new THREE.Vector2(1, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)];
+  genGeometry.faceVertexUvs[0].push(uvs2);
 
-    /******************************/
+  genGeometry.computeVertexNormals();
+  genGeometry.computeFaceNormals(); // Face normals are nice when you make a specific stylistic choice to show off the triangle itself
+  genGeometry.normalsNeedUpdate = true;
+
+  scene.add(genMesh);
+
+  // Controls
+  let controls = new OC.OrbitControls(fakeCamera, renderer.domElement);
   
 
+  /************************** FUNCTIONS **************************/
 
+  // Event listeners
+  window.addEventListener('resize', onWindowResize, false);
 
-    const cubes = [
-      makeInstance(geometry, 0x44aa88,  0),
-      makeInstance(geometry, 0x8844aa, -2),
-      makeInstance(geometry, 0xaa8844,  2),
-    ];
-
-    /* Cube rotation animation
-    function render(time) {
-      time *= 0.001;
-  
-      if (resizeRendererToDisplaySize(renderer)) {
-        const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-      }
-  
-      cubes.forEach((cube, ndx) => {
-        const speed = 1 + ndx * .1;
-        const rot = time * speed;
-        cube.rotation.x = rot;
-        cube.rotation.y = rot;
-        cube.rotation.z = rot;
-      });
-  
-      renderer.render(scene, camera);
-  
-      requestAnimationFrame(render);
-    }
-  
-    requestAnimationFrame(render);
-    */
-
-   let controls = new OC.OrbitControls( camera, renderer.domElement );
-   //controls.addEventListener('change', render);
-
-   window.addEventListener( 'resize', onWindowResize, false );
-
-function onWindowResize(){
+  // Window resize
+  function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+  };
 
-}
+  // Sphere animation
+  let updateSphere = function() {
+    let x = 1
+    let y = 1
+    let z = 1
 
+    let k = 3
 
-   // loop
-   function animate() {
- 
-    requestAnimationFrame(animate);
-    controls.update();
-    renderer.render(scene, camera);
+    let perlin2Value = noise.perlin2(x, y);
+    let perlin3Value = noise.perlin3(x, y, z);
 
-};
+    let noiseIntensityOverall = 0.3
+    let noiseScale = 1
+    let noiseIntensityComponents = 3
 
-animate();
+    let time = performance.now() * 0.001;
 
-}
+    for (let i = 0; i < sphere.geometry.faces.length; i++) { // Go through vertices/faces here and reposition them
 
+      let uv = sphere.geometry.faceVertexUvs[0][i]; //faceVertexUvs is a huge arrayed stored inside of another array
+      let f = sphere.geometry.faces[i];
+      let p = sphere.geometry.vertices[f.a];//take the first vertex from each face
+      
+      //let p = sphere.geometry.vertices[i];
+
+      /*p.normalize().multiplyScalar(noise.perlin3(p.x * noiseIntensityComponents + time,
+                                                 p.y * noiseIntensityComponents + time,
+                                                 p.z * noiseIntensityComponents + time)
+                                                     * noiseIntensityOverall + noiseScale);
+      */
+
+     p.normalize().multiplyScalar(noise.perlin3(uv[0].x * noiseIntensityComponents,
+                                                uv[0].y * noiseIntensityComponents,
+                                                time)   * noiseIntensityOverall + noiseScale);
+    }
+
+    sphere.geometry.computeVertexNormals();
+
+    sphere.geometry.verticesNeedUpdate = true; // Must be set or vertices will not update
+    sphere.geometry.normalsNeedUpdate = true;
+  }
+
+  // Cube animation
+  function animateCube(rotSpeed, geometry) {
+
+      rotSpeed = rotSpeed / 180 * pi
+    
+      if (geometry.rotation.x >= pi * 2) {
+        geometry.rotation.x = 0;
+        geometry.rotation.y = 0;
+        geometry.rotation.z = 0;
+      }
+      else {
+        geometry.rotation.x += rotSpeed;
+        geometry.rotation.y += rotSpeed;
+        geometry.rotation.z += rotSpeed;
+      }
+
+  };
+
+  // Sphere animation
+  function animateSphere(rotSpeed, geometry) {
+
+    updateSphere();
+
+  };
+
+  // Rendering
+  function render() {
+
+    //animateCube(rotSpeed, cubes[0]);
+
+    animateSphere();
   
-  main();
+    requestAnimationFrame(render);
+    controls.update();
+    renderer.render(scene, fakeCamera);
+  
+  };
+
+  render();
+
+}
+
+main();
