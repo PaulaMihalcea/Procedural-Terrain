a892c702c800862aabaddbd4da1c7df0161f3cbc
PaulaMihalcea <mihalceapaula@gmail.com>
2020-08-15 13:51:56 +0200

    Added 3x3 terrain generation loop; aligned tile borders

--- main.js
+++ main.js
@@ -28,8 +28,9 @@ function main() {
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
+    camera.position.x = 0.5;
     camera.position.y = 0.8;
-    camera.position.z = 1;
+    camera.position.z = 2; // TODO 1
     //camera.rotation.x = -1 * Math.PI / 180; // TODO
 
     // Fake camera
@@ -38,7 +39,7 @@ function main() {
 
     // Stats
     var stats = new Stats();
-    stats.showPanel( 0 );
+    stats.showPanel(0);
     document.body.appendChild( stats.dom );
 
     // Light
@@ -55,46 +56,115 @@ function main() {
     scene.background = new THREE.Color(0xffffff);
     scene.add(light);
 
-    // Plane 1
-    var geometry = new THREE.PlaneBufferGeometry(2, 2, 256, 256);
-    var material = new THREE.MeshLambertMaterial({ color: 0x3c3951 });
+    // Terrain parameters
+    noise.seed(6);
 
-    var peak = 0.1;
-    var smoothing = 0.3;
+    let maxHeight = 0.2; // Maximum terrain height
+    let smoothness = 0.3; // Terrain smoothness
+
+    const tileOffset = 2;
+    let tileSize = 256;
+    let terrainColor = 0x386653; // Terrain base color
 
     
-    var terrain = new THREE.Mesh(geometry, material);
-    terrain.rotation.x = -Math.PI / 2;
-    
-    scene.add(terrain);
+    // Plane matrix
+    const attributes = 3;
 
-    var vertices = terrain.geometry.attributes.position.array;
+    var geometry = []
+    var material = [];
+    let terrain = [];
+    var vertices = [];
 
-    for (var i = 0; i <= vertices.length; i += 3) {
-        vertices[i + 2] = peak * noise.perlin2(
-        vertices[i]/smoothing, 
-        vertices[i+1]/smoothing
-        );
+    var xOffset;
+    var zOffset;
+
+    for(let i=0; i<9; i++) {
+        geometry[i] = [];
+        material[i] = [];
+        terrain[i] = [];
+        vertices[i] = [];
     }
 
-    terrain.geometry.attributes.position.needsUpdate = true;
-    terrain.geometry.computeVertexNormals();
+
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            geometry[i][j] = new THREE.PlaneBufferGeometry(tileOffset, tileOffset, tileSize, tileSize);
+            material[i][j] = new THREE.MeshLambertMaterial({color: terrainColor});
+            
+            xOffset = tileOffset * i;
+            zOffset = tileOffset * j;
+
+            terrain[i][j] = new THREE.Mesh(geometry[i][j], material[i][j]);
+            
+            terrain[i][j].position.x += xOffset;
+            terrain[i][j].position.z += zOffset;
+            
+            terrain[i][j].rotation.x = -Math.PI / 2;
+
+            scene.add(terrain[i][j]);
+
+            vertices[i][j] = terrain[i][j].geometry.attributes.position.array;
+
+            for (let k = 0; k <= vertices[i][j].length; k += 3) {
+                vertices[i][j][k + 2] = noise.perlin2(
+                                        (vertices[i][j][k] + terrain[i][j].position.x) / smoothness,
+                                        (vertices[i][j][k + 1] - terrain[i][j].position.z) / smoothness)
+                                        * maxHeight;
+            }
+            terrain[i][j].geometry.attributes.position.needsUpdate = true;
+            terrain[i][j].geometry.computeVertexNormals();
+        }
+    }
+
+
+
+
+
+
+
+
+    /*
+    // Plane 1
+    let geometry1 = new THREE.PlaneBufferGeometry(tileOffset, tileOffset, 256, 256);
+    let material1 = new THREE.MeshLambertMaterial({color: terrainColor});
+    let terrain1 = new THREE.Mesh(geometry1, material1);
+
+    terrain1.rotation.x = -Math.PI / 2;
+    terrain1.position.x = 10;
+
+    console.log(terrain1.position.x)
     
+    scene.add(terrain1);
+
+    let vertices1 = terrain1.geometry.attributes.position.array;
+
+    //console.log(vertices)
+
+    for (let i = 0; i <= vertices1.length; i += 3) {
+        //console.log(vertices[i])
+        vertices1[i + 2] = noise.perlin2(vertices1[i]/smoothness, vertices1[i + 1]/smoothness) * maxHeight;
+    }
+
+    terrain1.geometry.attributes.position.needsUpdate = true;
+    terrain1.geometry.computeVertexNormals();
+    /*
 
     // Plane 2
-    var terrain2 = new THREE.Mesh(geometry, material);
+    let geometry2 = new THREE.PlaneBufferGeometry(tileLength, tileLength, 256, 256);
+    let material2 = new THREE.MeshLambertMaterial({color: 0xfffff});
+    var terrain2 = new THREE.Mesh(geometry2, material2);
     terrain2.rotation.x = -Math.PI / 2;
 
     
-    terrain2.position.x += 2;
-    //terrain2.position.y += 0.5;
+    terrain2.position.x += tileLength;
+    //terrain2.position.y += 0.5; // TODO
     
     var vertices2 = terrain2.geometry.attributes.position.array;
 
     for (var i = 0; i <= vertices2.length; i += 3) {
-        vertices2[i + 2] = peak * noise.perlin2(
-        vertices2[i]/smoothing, 
-        vertices2[i+1]/smoothing
+        vertices2[i + 2] = maxHeight * noise.perlin2(
+            (vertices2[i] + terrain2.position.x) /smoothness, 
+            (vertices2[i+1] + terrain2.position.y) /smoothness
         );
     }
 
@@ -103,7 +173,8 @@ function main() {
 
 
     scene.add(terrain2);
-
+    console.log(terrain2.position.x)
+    */
 
 
 
@@ -139,9 +210,9 @@ function main() {
     function refreshVertices() {
         var vertices = terrain.geometry.attributes.position.array;
         for (var i = 0; i <= vertices.length; i += 3) {
-            vertices[i+2] = peak * noise.perlin2(
-                (terrain.position.x + vertices[i])/smoothing, 
-                (terrain.position.z + vertices[i+1])/smoothing
+            vertices[i+2] = maxHeight * noise.perlin2(
+                (terrain.position.x + vertices[i])/smoothness, 
+                (terrain.position.z + vertices[i+1])/smoothness
             );
         }
         terrain.geometry.attributes.position.needsUpdate = true;
